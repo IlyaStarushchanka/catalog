9845931208b5e0bb5202d1941d34ee05ff9f4b01
Ilya Starushchenko <ilya.starushchenko@gambinoslot.com>
2021-07-07 16:36:59 +0300

    ignore case changes on the search input

--- src/main/java/com/ilya/catalog/facade/CatalogFacade.java
+++ src/main/java/com/ilya/catalog/facade/CatalogFacade.java
@@ -74,11 +74,11 @@ public class CatalogFacade {
 
     public List<String> getContestNames(String search){
         List<String> names = catalogService.getContestNames(search);
-        List<String> result = new ArrayList<>(5);
+        List<String> result = new ArrayList<>(3);
         for (String name : names){
             String replacement = "<mark>" + search + "</mark>";
             result.add(name.replaceFirst("(?i)"+search, replacement));
-            if (result.size() == 5){
+            if (result.size() == 3){
                 break;
             }
         }
